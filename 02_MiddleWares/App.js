f9bccecd4adf793a83e15c8d3e905bed30c0a728
Krishnendu <krishnendu.kalyani1@gmail.com>
2022-05-31 00:39:47 +0530

    Middleware added

---
+++ 02_MiddleWares/App.js
@@ -0,0 +1,35 @@
+// Step 1: npm init -y
+// Step 2: npm i express
+// Step 3: Create App.js
+
+
+//Express
+const express = require('express');
+const app = express();
+
+// MiddleWare
+const middleware = (req, res, next) =>{
+    console.log(`Hello there, I am Middleware`);
+}
+
+
+// Create route
+// app.get(path, callback)
+app.get('/', (req, res) =>{
+    res.send(`Hello there, I am krishnendu roy`);
+});
+
+app.get('/about', middleware, (req, res) =>{
+    res.send(`Hello there, What you want to know about krishnendu?`);
+});
+
+app.get('/contact', (req, res) =>{
+    res.send(`Hello there, Why you want to contact krishnendu?`);
+});
+
+
+
+// Listen port
+app.listen(3000, ()=>{
+    console.log(`Server is running`);
+})
