230c3da6f80b9990946617c5d9c591c987e1b8e0
Krishnendu <krishnendu.kalyani1@gmail.com>
2022-05-31 13:02:54 +0530

    connection

---
+++ 03_connect_backend_to_database/App.js
@@ -0,0 +1,50 @@
+// Step 1: npm init -y
+// Step 2: npm i express
+// Step 3: Create App.js
+
+// Mongoose 
+
+const mongoose = require('mongoose');
+
+//Express
+const express = require('express');
+const app = express();
+
+// Database
+const DB = 'mongodb+srv://root:toor@cluster0.qz4sg1h.mongodb.net/mernstacklearn?retryWrites=true&w=majority'
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() =>{
+    console.log(`Connection successful`);
+}).catch((err) => console.log(`err`));
+
+
+// Middleware
+const middleware = (req, res, next) =>{
+    console.log(`Hello there, i am middleware`);
+    next();
+}
+
+// Create route
+// app.get(path, callback)
+app.get('/', (req, res) =>{
+    res.send(`Hello there, I am krishnendu roy`);
+});
+
+app.get('/about', middleware, (req, res) =>{
+    res.send(`Hello there, What you want to know about krishnendu?`);
+});
+
+app.get('/contact', (req, res) =>{
+    res.send(`Hello there, Why you want to contact krishnendu?`);
+});
+
+
+
+// Listen port
+app.listen(3000, ()=>{
+    console.log(`Server is running`);
+});
+
+//Krishnendu roy
